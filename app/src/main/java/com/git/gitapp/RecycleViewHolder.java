247ecbd264645df237a377025c21c8abb619fb9c
aneri135 <37016988+aneri135@users.noreply.github.com>
2019-03-29 00:00:20 +0530

    Revert "navo-code"

--- app/src/main/java/com/git/gitapp/RecycleViewHolder.java
+++ app/src/main/java/com/git/gitapp/RecycleViewHolder.java
@@ -3,7 +3,6 @@ package com.git.gitapp;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -14,8 +13,6 @@ class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
     TextView cDate, cTitle, cUrl;
     ImageView imageView;
-    RelativeLayout layout;
-
 
     public RecyclerViewHolder(View itemView) {
         super(itemView);
@@ -24,7 +21,5 @@ class RecyclerViewHolder extends RecyclerView.ViewHolder {
         cTitle = (TextView) itemView.findViewById(R.id.cTitle);
         imageView = (ImageView) itemView.findViewById(R.id.list_avatar);
 //        cUrl=(ImageView) itemView.findViewById(R.id.cUrl);
-        layout = (RelativeLayout)itemView.findViewById(R.id.layout);
-
     }
 }
